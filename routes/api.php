3ffb1613c7eb3442ce17ec257bfbae647d0858af
hazim.hadis <hazim.hadis@gmail.com>
2022-07-23 05:09:19 +0800

    single file queue

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MessageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::apiResource('messages', MessageController::class, ['only' => ['index', 'store', 'show']]);
+Route::get('/messages', [MessageController::class, 'index']);
+Route::post('/messages/store', [MessageController::class, 'store']);
+Route::get('/messages/consume', [MessageController::class, 'consume']);
+Route::get('/messages/total', [MessageController::class, 'total']);
